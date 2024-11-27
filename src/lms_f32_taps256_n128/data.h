6de5ba32a25d56624a6174c9ca22a78848896954
jenniferhellar <50834119+jenniferhellar@users.noreply.github.com>
2024-11-26 21:11:39 -0600

    adding lms_f32 test

---
+++ src/lms_f32_taps256_n128/data.h
@@ -0,0 +1,20 @@
+
+#ifndef DATA_H
+#define DATA_H
+
+#include "arm_math.h"
+
+#define N_TAPS          (256)
+#define TOTAL_SAMPLES   (384)
+#define N_SAMPLES       (128)
+#define SNR_REF_THLD    (80)
+
+#define N_INITIAL       (TOTAL_SAMPLES-N_SAMPLES)
+
+static float32_t mu =  0.0500000000f;
+
+extern float32_t input[TOTAL_SAMPLES];
+extern float32_t ref[TOTAL_SAMPLES];
+extern float32_t output_ref[TOTAL_SAMPLES];
+
+#endif  // DATA_H
